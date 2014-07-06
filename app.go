f6eb1331b2de1422c22c910d97246adb2a9a7f6b
Hiroaki Nakamura <hnakamur@gmail.com>
2014-07-07 00:00:30 +0900

    Add User model

--- app.go
+++ app.go
@@ -1,14 +1,28 @@
-package hello
+package timecard
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
 	"time"
 
 	"appengine"
 	"appengine/datastore"
 	"appengine/user"
 )
+
+type User struct {
+	Email   string
+	Name    string
+	Enabled bool
+}
+
+func userKey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "User", "default_user", 0, nil)
+}
 
 type Punch struct {
 	Puncher string
@@ -36,6 +50,7 @@ func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if u == nil {
 		url, err := user.LoginURL(c, r.URL.String())
 		if err != nil {
+			c.Errorf("%v", err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
@@ -44,9 +59,43 @@ func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if e := fn(c, w, r); e != nil {
-		c.Errorf("%v", e.Error)
-		http.Error(w, e.Message, e.Code)
+		handleAppError(c, w, e)
 	}
+}
+
+func handleAppError(c appengine.Context, w http.ResponseWriter, e *appError) {
+	c.Errorf("%v", e.Error)
+	http.Error(w, e.Message, e.Code)
+}
+
+type apiHandler func(appengine.Context, http.ResponseWriter, *http.Request) (jsonData interface{}, error *appError)
+
+func (fn apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		err := errors.New("login needed")
+		c.Errorf("%v", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	jsonData, appErr := fn(c, w, r)
+	if appErr != nil {
+		handleAppError(c, w, appErr)
+	}
+
+	err := writeJsonResponse(w, jsonData)
+	if err != nil {
+		c.Errorf("%v", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func writeJsonResponse(w http.ResponseWriter, jsonData interface{}) error {
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	return encoder.Encode(jsonData)
 }
 
 func redirect(w http.ResponseWriter, url string) {
@@ -58,6 +107,8 @@ func init() {
 	http.Handle("/", appHandler(rootHandler))
 	http.Handle("/my/arrivals", appHandler(myArrivalsHandler))
 	http.Handle("/my/leaves", appHandler(myLeavesHandler))
+
+	http.Handle("/api/admin/users", apiHandler(apiAdminUsersHandler))
 }
 
 func rootHandler(c appengine.Context, w http.ResponseWriter, r *http.Request) *appError {
@@ -154,4 +205,85 @@ func createPunch(c appengine.Context, punchType string) *appError {
 		}
 	}
 	return nil
+}
+
+func apiAdminUsersHandler(c appengine.Context, w http.ResponseWriter, r *http.Request) (interface{}, *appError) {
+	if r.Method == "GET" {
+		q := datastore.NewQuery("User").Ancestor(punchKey(c)).Order("Name")
+		var users []User
+		if _, err := q.GetAll(c, &users); err != nil {
+			return nil, &appError{
+				Error:   err,
+				Message: "Failed to fetch users data from the datastore",
+				Code:    http.StatusInternalServerError,
+			}
+		}
+
+		var jsonUsers []interface{}
+		for _, user := range users {
+			jsonUsers = append(jsonUsers, map[string]interface{}{
+				"email":   user.Email,
+				"name":    user.Name,
+				"enabled": user.Enabled,
+			})
+		}
+
+		return map[string]interface{}{
+			"users": jsonUsers,
+		}, nil
+
+	} else if r.Method == "POST" {
+		enabled, appErr := getFormBoolValue(r, "enabled", true)
+		if appErr != nil {
+			return nil, appErr
+		}
+
+		c.Debugf("formvalues. email=%s, name=%s", r.FormValue("email"), r.FormValue("name"))
+		u := User{
+			Email:   r.FormValue("email"),
+			Name:    r.FormValue("name"),
+			Enabled: enabled,
+		}
+		key := datastore.NewIncompleteKey(c, "User", punchKey(c))
+		_, err := datastore.Put(c, key, &u)
+		if err != nil {
+			return nil, &appError{
+				Error:   err,
+				Message: "Failed to put a user data to the datastore",
+				Code:    http.StatusInternalServerError,
+			}
+		}
+
+		return map[string]interface{}{
+			"user": map[string]interface{}{
+				"email":   r.FormValue("email"),
+				"name":    u.Name,
+				"enabled": u.Enabled,
+			},
+		}, nil
+	} else {
+		err := errors.New("Unsupported http method")
+		return nil, &appError{
+			Error:   err,
+			Message: err.Error(),
+			Code:    http.StatusBadRequest,
+		}
+	}
+}
+
+func getFormBoolValue(r *http.Request, name string, defaultValue bool) (bool, *appError) {
+	boolValue := defaultValue
+	strValue := r.FormValue(name)
+	if strValue != "" {
+		var err error
+		boolValue, err = strconv.ParseBool(strValue)
+		if err != nil {
+			return false, &appError{
+				Error:   err,
+				Message: fmt.Sprintf(`Failed to parse the "%s" parameter as a boolean value`, name),
+				Code:    http.StatusBadRequest,
+			}
+		}
+	}
+	return boolValue, nil
 }
